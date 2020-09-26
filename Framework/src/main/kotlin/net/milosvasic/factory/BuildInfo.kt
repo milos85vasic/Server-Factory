40153654a240d5002320a82a862ec3a481642104
Milos Vasic <milos85vasic@gmail.com>
2020-09-26 18:39:56 +0200

    MSF-215 - Decoupling the application.

--- Framework/src/main/kotlin/net/milosvasic/factory/BuildInfo.kt
+++ Framework/src/main/kotlin/net/milosvasic/factory/BuildInfo.kt
@@ -2,7 +2,7 @@ package net.milosvasic.factory
 
 object BuildInfo {
 
-    const val NAME = "Server-Factory"
+    const val NAME = "Server Factory Framework"
     const val VERSION = "1.0.0"
     const val VERSION_CODE = (100 * 1000) + 0
 }
