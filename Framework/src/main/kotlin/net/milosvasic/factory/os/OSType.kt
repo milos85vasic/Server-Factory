7adaaef4faafb6d65081c450051f59b963a520da
Milos Vasic <milos85vasic@gmail.com>
2020-10-18 11:36:11 +0200

    MSF-29 - Server OS types added.

--- Framework/src/main/kotlin/net/milosvasic/factory/os/OSType.kt
+++ Framework/src/main/kotlin/net/milosvasic/factory/os/OSType.kt
@@ -6,8 +6,10 @@ enum class OSType(val osName: String) {
     CENTOS_8(CENTOS.osName),
     CENTOS_7("CentOS_7"),
     UBUNTU("Ubuntu"),
+    UBUNTU_SERVER("Ubuntu_Server"),
     DEBIAN("Debian"),
     FEDORA("Fedora"),
+    FEDORA_SERVER("Fedora_Server"),
     REDHAT("RedHat"),
     UNKNOWN("Unknown")
 }
