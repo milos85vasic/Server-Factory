9d6771402460dea09389856d8fed9bba68e7311b
Milos Vasic <milos85vasic@gmail.com>
2020-12-16 14:23:54 +0100

    MSF-233 - Common server factory - First basic version.

---
+++ Framework/src/main/kotlin/net/milosvasic/factory/application/server_factory/common/CommonServerFactoryServerConfigurationFactory.kt
@@ -0,0 +1,21 @@
+package net.milosvasic.factory.application.server_factory.common
+
+import com.google.gson.reflect.TypeToken
+import net.milosvasic.factory.configuration.ConfigurationFactory
+import net.milosvasic.factory.log
+import java.lang.reflect.Type
+
+class CommonServerFactoryServerConfigurationFactory : ConfigurationFactory<CommonServerFactoryConfiguration>() {
+
+    override fun getType(): Type {
+
+        return object : TypeToken<CommonServerFactoryConfiguration>() {}.type
+    }
+
+    override fun validateConfiguration(configuration: CommonServerFactoryConfiguration) = true
+
+    override fun onInstantiated(configuration: CommonServerFactoryConfiguration) {
+
+        log.v("Instantiated, configuration: $configuration")
+    }
+}
